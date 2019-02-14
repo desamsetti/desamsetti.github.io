f7c8bcaeacb844012a57ca84a28a8417e4d8f319
Harshith Desamsetti <dharshith1996@gmail.com>
2019-02-14 16:41:19 -0600

    Updated

--- Projects/JavaScript/ToDoList Project/assets/js/todos.js
+++ Projects/JavaScript/ToDoList Project/assets/js/todos.js
@@ -3,7 +3,7 @@ $("ul").on("click","li",function(){
 	$(this).toggleClass("completed");
 })
 
-
+/*
 //Click on X to delete Todo
 $("ul").on("click","span",function(event){	
 	event.stopPropagation();
@@ -18,7 +18,7 @@ $("ul").on("click","span",function(event){
 	sound.play();	
 
 });
-
+*/
 
 $("input[type='text'").keypress(function(event){
 	if(event.which === 13)
